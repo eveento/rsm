b6c4bc4b7635790552bca16d87db25b590b639e1
evento <evento@onet.eu>
2018-05-19 16:02:36 +0200

    inicjalizacja projektu na rsm

--- rms_load_balancer/src/main/java/data/Application.java
+++ rms_load_balancer/src/main/java/data/Application.java
@@ -1,10 +1,18 @@
 package data;
 
+import java.awt.EventQueue;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ViewController();
+			}
+		});
 	}
 
 }
---
+++ rms_load_balancer/src/main/java/data/ViewController.java
@@ -0,0 +1,13 @@
+package data;
+
+import javax.swing.JFrame;
+
+public class ViewController extends JFrame{
+	
+	public ViewController() {
+		super("Load Balancer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
