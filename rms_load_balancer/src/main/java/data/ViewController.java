ad5c34a33b70f8776ea564aad2165d673661e4a3
evento <evento@onet.eu>
2018-05-19 16:09:19 +0200

    inicjalizacja projektu na rsm

--- rms_load_balancer/src/main/java/data/ViewController.java
+++ rms_load_balancer/src/main/java/data/ViewController.java
@@ -7,6 +7,8 @@ public class ViewController extends JFrame{
 	public ViewController() {
 		super("Load Balancer");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700, 450);
+		setLocation(50,50);
 		setVisible(true);
 	}
 
