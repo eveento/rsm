58c38d9f688620b320654b1ee663538766340505
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-05-19 19:22:14 +0200

    Created Request Class

---
+++ rms_load_balancer/src/main/java/domain/Request.java
@@ -0,0 +1,55 @@
+package domain;
+
+import java.util.Random;
+
+public class Request {
+	static int numerator = 0;
+	private final int requestId;
+	
+	private final double initialWorkToDo;
+	private double remainingWorkToDo;
+	
+	private String address;
+	
+	private final String defaultAddress = "192";
+	
+	public Request(double _initialWorkToDo)
+	{
+		requestId = ++numerator;
+		
+		initialWorkToDo = _initialWorkToDo;
+		remainingWorkToDo = initialWorkToDo;
+		
+		setAddress();
+	}
+	
+	public boolean CheckIfDone()
+	{
+		return remainingWorkToDo <= 0;
+	}
+	
+	public void Work(double performanceFactor)
+	{
+		if(!CheckIfDone()) 
+		{
+			remainingWorkToDo -= performanceFactor;
+		}
+	}
+	
+	public boolean WorkAndCheckIfDone(double performanceFactor)
+	{
+		Work(performanceFactor);
+		return CheckIfDone();
+	}
+	
+	private void setAddress()
+	{
+		Random rand = new Random();
+		String first = defaultAddress;
+		//**** od 0 do 255 chyba
+		int second = rand.nextInt(256);
+		int third = rand.nextInt(256);
+		int fourth = rand.nextInt(256);
+		address = first +"."+String.valueOf(second)+"."+String.valueOf(third)+"."+String.valueOf(fourth);
+	}
+}
