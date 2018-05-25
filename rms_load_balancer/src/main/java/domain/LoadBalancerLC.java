a2bbb086fd015253c28b5418edd320580885d2dd
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-05-25 21:53:32 +0200

    LeastConnections load balancer should work

--- rms_load_balancer/src/main/java/domain/LoadBalancerLC.java
+++ rms_load_balancer/src/main/java/domain/LoadBalancerLC.java
@@ -23,6 +23,34 @@ public class LoadBalancerLC extends LoadBalancer
 	@Override
 	public void Work()
 	{
+		int chosenServerIndex = 0;
+		while (CheckIfContinueWork())
+		{
+			chosenServerIndex = ChooseIndexOfTheServerWithTheLeastConnections();
+			if (ListOfServers.get(chosenServerIndex).CheckIfCanAcceptRequest())
+			{
+				ListOfServers.get(chosenServerIndex).AddRequest(QueueOfRequests.poll());
+			}
+		}
+	}
 
+	private int ChooseIndexOfTheServerWithTheLeastConnections()
+	{
+		int index = 0;
+		int numberOfConnections = Integer.MAX_VALUE;
+
+		for (int i = 0; i < ListOfServers.size(); i++)
+		{
+			if (ListOfServers.get(i).GetNumberOfRequestsBeingServed() < numberOfConnections)
+			{
+				if (ListOfServers.get(i).CheckIfCanAcceptRequest())
+				{
+					index = i;
+					numberOfConnections = ListOfServers.get(i).GetNumberOfRequestsBeingServed();
+				}
+			}
+		}
+
+		return index;
 	}
 }
--- rms_load_balancer/src/main/java/domain/ServerUnit.java
+++ rms_load_balancer/src/main/java/domain/ServerUnit.java
@@ -29,6 +29,11 @@ public class ServerUnit
 		WorkOnRequests();
 		RemoveDoneRequests();
 	}
+	
+	public int GetNumberOfRequestsBeingServed()
+	{
+		return ListOfRequests.size();
+	}
 
 	public boolean AddRequest(Request request)
 	{
