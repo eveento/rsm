f7ae64a9f2c2bea82556656cacc17821d3e3d368
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-05-25 21:20:42 +0200

    Removed 'test' class

--- rms_load_balancer/src/main/java/data/Application.java
+++ rms_load_balancer/src/main/java/data/Application.java
@@ -21,8 +21,7 @@ public class Application {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		test test1 = new test();
-		test1.Run();
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
