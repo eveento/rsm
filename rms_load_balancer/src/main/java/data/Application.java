67edd2e3b7e6195921010e390523317b14926b9d
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-05-29 17:21:45 +0200

    NOW Created buttons to run simulation

--- rms_load_balancer/src/main/java/data/Application.java
+++ rms_load_balancer/src/main/java/data/Application.java
@@ -10,6 +10,11 @@ import java.util.Random;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
 import javax.swing.SpringLayout;
+import javax.swing.BoxLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Application
 {
@@ -71,5 +76,45 @@ public class Application
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SpringLayout springLayout = new SpringLayout();
 		frame.getContentPane().setLayout(springLayout);
+		
+		JButton btnX = new JButton("x1");
+		springLayout.putConstraint(SpringLayout.WEST, btnX, 457, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnX, -10, SpringLayout.SOUTH, frame.getContentPane());
+		frame.getContentPane().add(btnX);
+		
+		JButton btnX_1 = new JButton("x10");
+		springLayout.putConstraint(SpringLayout.NORTH, btnX, 0, SpringLayout.NORTH, btnX_1);
+		springLayout.putConstraint(SpringLayout.EAST, btnX, -6, SpringLayout.WEST, btnX_1);
+		springLayout.putConstraint(SpringLayout.WEST, btnX_1, 550, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnX_1, -10, SpringLayout.SOUTH, frame.getContentPane());
+		frame.getContentPane().add(btnX_1);
+		
+		JButton btnX_2 = new JButton("x100");
+		springLayout.putConstraint(SpringLayout.NORTH, btnX_1, 0, SpringLayout.NORTH, btnX_2);
+		springLayout.putConstraint(SpringLayout.EAST, btnX_1, -6, SpringLayout.WEST, btnX_2);
+		springLayout.putConstraint(SpringLayout.WEST, btnX_2, 643, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(btnX_2);
+		
+		JButton btnAll = new JButton("All");
+		springLayout.putConstraint(SpringLayout.NORTH, btnAll, 427, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnAll, 740, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnAll, -10, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnAll, -10, SpringLayout.EAST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnX_2, 0, SpringLayout.NORTH, btnAll);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnX_2, 0, SpringLayout.SOUTH, btnAll);
+		springLayout.putConstraint(SpringLayout.EAST, btnX_2, -10, SpringLayout.WEST, btnAll);
+		frame.getContentPane().add(btnAll);
+		
+		JLabel lblRun = new JLabel("Run simulation steps");
+		springLayout.putConstraint(SpringLayout.WEST, lblRun, 0, SpringLayout.WEST, btnX);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblRun, -6, SpringLayout.NORTH, btnX);
+		lblRun.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		frame.getContentPane().add(lblRun);
+		
+		JButton btnRunOnceOne = new JButton("Run once one request");
+		springLayout.putConstraint(SpringLayout.NORTH, btnRunOnceOne, 0, SpringLayout.NORTH, btnX);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnRunOnceOne, 0, SpringLayout.SOUTH, btnX);
+		springLayout.putConstraint(SpringLayout.EAST, btnRunOnceOne, -6, SpringLayout.WEST, btnX);
+		frame.getContentPane().add(btnRunOnceOne);
 	}
 }
